3545c338a91fd2ee942e654a602e2b9ef235cb2d
HoSyVinh-1510 <babyvinh46@gmail.com>
2026-06-06 10:06:43 +0700

    Tao solution chuc nang Finance gom: Entities, DbContext

---
+++ FinanceService/Entities/IndividualDonation.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace FinanceService.Entities
+{
+    public class IndividualDonation
+    {
+        public int Id { get; set; }
+        public int CampaignId { get; set; }
+        public int UserId { get; set; }
+        public decimal Amount { get; set; }
+        public string DisplayName { get; set; } = "";
+        public string Phone { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Note { get; set; } = "";
+        public bool IsAnonymous { get; set; }
+        public string ProofImageUrl { get; set; } = "";
+        public string Status { get; set; } = "PendingConfirmation";
+        public int? ConfirmedBy { get; set; }
+        public DateTime? ConfirmedAt { get; set; }
+        public string RejectedReason { get; set; } = "";
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+
+        public SupportCampaign Campaign { get; set; }
+    }
+}
